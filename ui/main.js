b45d15a031bdd35763ad09a66723fe009419ebf2
dheerajthodupunoori <dheeraj.thodupunoori01@gmail.com>
2016-09-23 12:54:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,6 +34,6 @@ submit.onClick = function(){
     for(var i=0;i<names.length;i++){
         list += '<li>'+ names[i] + '</li>';
     }
-    var ui = document.getElementById('namelist');
-    ui.innerHTML = list;
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
 };
