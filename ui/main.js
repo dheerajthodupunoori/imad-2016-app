d9e348e1f9b45d63685e8d1c4e2cd537c3dc3ca5
dheerajthodupunoori <dheeraj.thodupunoori01@gmail.com>
2016-09-23 12:25:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,3 @@
-/*console.log('loaded!');
-//change the text of the main-text in div
-var element=document.getElementById('main-text');
-element.innerHTML='DHEERAJ';
-
-//move the image
-var image = document.getElementById('img');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft = marginLeft+10;
-    image.style.marginLeft = marginLeft+'px';
-}
-image.onClick = function(){
-    var interval = setInterval(moveRight,100);
-};*/
-
-
 //var counter = 0;
 var button = document.getElementById('counter');
 button.onClick = function(){
