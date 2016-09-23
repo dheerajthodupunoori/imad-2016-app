7392fec456b5eccf59e04e58d8b0ce2dc63747d7
dheerajthodupunoori <dheeraj.thodupunoori01@gmail.com>
2016-09-23 12:16:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,16 +15,24 @@ image.onClick = function(){
 };*/
 
 
-var counter = 0;
+//var counter = 0;
 var button = document.getElementById('counter');
 button.onClick = function(){
-    //
-    
-    
-    
-    
-   //render the variable in the correct span 
-    counter = counter+1;
-    var span  = document.getElementById('count');
-    span.innerHTML = counter.toString();
-}
+    //create a request to the counter enddpoint
+    var request = new XMLHttpRequest();
+    //cpature the response and store it in the counter varaiable
+    request.onreadystatechange = function(){
+        if(request.readystate==XMLHttpRequest.DONE){
+            if(request.status==200){
+            var counter = request.responseText;
+             var span  = document.getElementById('count');
+             span.innerHTML = counter.toString();
+            }
+        }
+        
+        
+    };
+    //make a request
+    request.Open('GET','http://http://dheerajthodupunoori.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
